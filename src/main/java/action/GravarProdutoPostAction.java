a3f536b1b6626ba5c496a0306149c894174de761
Rian Alves <rianalves2009.ra@gmail.com>
2018-10-25 01:43:03 -0200

    Implementação : >>GravarProdutoPostAction >>GravarProdutoAction

---
+++ src/main/java/action/GravarProdutoPostAction.java
@@ -0,0 +1,21 @@
+
+package action;
+
+import controller.Action;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Rian Alves
+ */
+public class GravarProdutoPostAction implements Action {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

3c44adf7fc74e55488f031fc09ea47eec1d524d8
Rian Alves <rianalves2009.ra@gmail.com>
2018-10-25 12:32:38 -0200

    Implementação : >ACTION: >>ListarProduto >>ListarUsuario >>ListarRestaurante >>RestauranteStatePedidoAnterior >>RestauranteStatePedidoPosterior >>StateAguardandoPedido >>StateConcluido >>StateConfirmaPedido >>StatePreparandoPedido >>StateSaindoEntrega

---
+++ src/main/java/action/StateConfirmaPedidoAction.java
@@ -0,0 +1,21 @@
+
+package action;
+
+import controller.Action;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Rian Alves
+ */
+public class StateConfirmaPedidoAction implements Action {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
+       
+    }
+    
+}

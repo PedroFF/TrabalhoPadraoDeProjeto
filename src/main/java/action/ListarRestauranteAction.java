5f08a067dec9c72af7a38dbf9fdac8fd2f6bcef8
Rian Alves <rianalves2009.ra@gmail.com>
2018-11-04 12:57:35 -0200

    Implementação do ListarRestaurantes para página inicial

--- src/main/java/action/ListarRestauranteAction.java
+++ src/main/java/action/ListarRestauranteAction.java
@@ -3,11 +3,17 @@ package action;
 
 import controller.Action;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.UsuarioRestaurante;
+import persistence.UsuarioDAO;
 
 /**
  *
@@ -17,6 +23,15 @@ public class ListarRestauranteAction implements Action {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
+        try {
+            List<UsuarioRestaurante> restaurantes = new ArrayList<>();
+            restaurantes =  UsuarioDAO.getInstance().getAllUsuariosRestaurante();
+            request.setAttribute("restaurantes",restaurantes);
+            RequestDispatcher despachante = request.getRequestDispatcher("/ClienteInicio.jsp");
+            despachante.forward(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ListarRestauranteAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
        
        
     }

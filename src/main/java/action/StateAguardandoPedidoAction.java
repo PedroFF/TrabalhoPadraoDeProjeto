742d3487469b40a6fe90780d006aeffd2f0eba29
Pedro <soufreitas.pedro@gmail.com>
2018-11-05 00:27:35 -0200

    Fazendo o esboço do state

--- src/main/java/action/StateAguardandoPedidoAction.java
+++ src/main/java/action/StateAguardandoPedidoAction.java
@@ -7,19 +7,27 @@ package action;
 
 import controller.Action;
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.EstadoNaoPermitidoException;
+import model.Pedido;
+import persistence.PedidoDAO;
 
 /**
  *
  * @author Rian Alves
  */
-public class StateAguardandoPedidoAction implements Action{
+public class StateAguardandoPedidoAction implements Action {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
-        
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        Pedido pedido = (Pedido) PedidoDAO.getInstance().getByID(id);
+        pedido.saveToMemento();
+        pedido.PedidoDAO.getInstance().insertHistorico(pedido, pedido.restoreFromMemento(0).getEstadoSalvo().getNomeEstado(), false);
+        PedidoDAO.getInstance().updateEstado(pedido);
     }
-    
+
 }

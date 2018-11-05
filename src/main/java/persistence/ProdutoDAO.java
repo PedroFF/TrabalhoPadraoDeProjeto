3773b34a5d8eaeee0e26e503ac6e910eda3dc473
Joao Paulo <jpdias1997@hotmail.com>
2018-11-04 22:16:21 -0200

    Criação do GetAllProdutos por Restaurante

--- src/main/java/persistence/ProdutoDAO.java
+++ src/main/java/persistence/ProdutoDAO.java
@@ -17,33 +17,24 @@ import model.UsuarioRestaurante;
  * @author Rian Alves
  */
 public class ProdutoDAO {
-
+    
     private Connection conexao;
     private static final ProdutoDAO INSTANCE = new ProdutoDAO();
     private String SQL_INSERT_ITEM = "INSERT INTO item(id_item,nome,preco,fk_restaurante,ingrediente) VALUES (?,?,?,?,?)";
     private String SQL_INSERT_ITEM_INGREDIENTE = "INSERT INTO INGREDIENTE(id_item_composto,id_item_componente) VALUES (?,?)";
     private String SQL_SELECT_ITEM_BY_ID_AND_RESTAURANTE = "SELECT * FROM ITEM WHERE ID = ? AND FK_RESTAURANTE = ?";
-    private String SQL_SELECT_ALL_ITEMS = "SELECT * FROM ITEM WHERE FK_RESTAURANTE = ?";
+    private String SQL_SELECT_ALL_ITEMS_COMPOSTOS = "SELECT id_item FROM ITEM WHERE FK_RESTAURANTE = ? AND INGREDIENTE = FALSE";
     private String SQL_SELECT_ITEM_INGREDIENTES = "SELECT * FROM ITEM WHERE INGREDIENTE = TRUE AND FK_RESTAURANTE = ?";
     private String SQL_SELECT_INGREDIENTES = "SELECT id_item, fk_restaurante FROM ITEM inner join ingrediente on item.id_item = ingrediente.id_item_componente where id_item_composto = ?";
-
+    
     public static ProdutoDAO getINSTANCE() {
         return INSTANCE;
     }
-
-    public void save(Produto produto) throws SQLException, ClassNotFoundException {
-        Connection conn = null;
-        Statement st = null;
-
-        try {
-            conn = DatabaseLocator.getInstance().getConnection();
-            st = conn.createStatement();
-            st.execute("insert into empresa(nomeEmpresa) values ('" + produto + "')");
-        } catch (SQLException e) {
-            throw e;
-        }
+    
+    public void adicionar(Produto produto) throws SQLException, ClassNotFoundException {
+        
     }
-
+    
     public Produto getProdutoByID(Integer id_item, Integer id_restaurante) throws ClassNotFoundException, SQLException {
         Produto item = null;
         try (PreparedStatement consulta = conexao.prepareStatement(SQL_SELECT_ITEM_BY_ID_AND_RESTAURANTE)) {
@@ -66,7 +57,7 @@ public class ProdutoDAO {
         }
         return item;
     }
-
+    
     public List<Ingrediente> getIngredientes(Integer id_item) throws SQLException, ClassNotFoundException {
         List<Ingrediente> ingredientes = new ArrayList<>();
         try (PreparedStatement consulta = conexao.prepareStatement(SQL_SELECT_INGREDIENTES)) {
@@ -77,69 +68,24 @@ public class ProdutoDAO {
                 do {
                     ingredientes.add((Ingrediente) this.getProdutoByID(resultado.getInt("id_item"), resultado.getInt("fk_restaurante")));
                 } while (resultado.next());
-
+                
             }
         }
         return ingredientes;
     }
-
-    public List<Produto> getProdutos() throws SQLException, ClassNotFoundException {
-        Connection conn = null;
-        Statement st = null;
-
+    
+    public List<Produto> getAllProdutos(Integer id_restaurante) throws SQLException, ClassNotFoundException {
         List<Produto> produtos = new ArrayList<>();
-        try {
-            conn = DatabaseLocator.getInstance().getConnection();
-            String sql = "SELECT * FROM PRODUTO";
-            st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            while (rs.next()) {
-                produtos.add(new Produto());
+        try (PreparedStatement consulta = conexao.prepareStatement(SQL_SELECT_ALL_ITEMS_COMPOSTOS)) {
+            consulta.setInt(1, id_restaurante);
+            consulta.execute();
+            ResultSet resultado = consulta.executeQuery();
+            if (resultado.next()) {
+                do {
+                    produtos.add(this.getProdutoByID(resultado.getInt("id_item"), id_restaurante));
+                } while (resultado.next());
+                
             }
-        } catch (SQLException e) {
-            throw e;
-        }
-        return produtos;
-    }
-
-    public void delete(Produto produto) throws ClassNotFoundException, SQLException {
-        Connection conn = null;
-        Statement st = null;
-
-        try {
-            conn = DatabaseLocator.getInstance().getConnection();
-            st = conn.createStatement();
-            st.execute("DELETE FROM empresa WHERE nomeEmpresa ='" + produto + "'");
-        } catch (SQLException e) {
-            throw e;
-        }
-    }
-
-    public void update(Produto produto) throws ClassNotFoundException, SQLException {
-        Connection conn = null;
-        Statement st = null;
-
-        try {
-            conn = DatabaseLocator.getInstance().getConnection();
-            st = conn.createStatement();
-            st.execute("UPDATE empresa SET nomeEmpresa ='" + produto + "' WHERE id =");
-        } catch (SQLException e) {
-            throw e;
-        }
-    }
-
-    public List<Produto> listAll() throws SQLException, ClassNotFoundException {
-        Connection conn = null;
-        Statement st = null;
-        List<Produto> produtos = new ArrayList<>();
-        conn = DatabaseLocator.getInstance().getConnection();
-        PreparedStatement consulta = conn.prepareStatement("Select * from produto");
-        ResultSet resultado = consulta.executeQuery();
-        if (resultado.next()) {
-            do {
-                produtos.add(new Produto());
-
-            } while (resultado.next());
         }
         return produtos;
     }

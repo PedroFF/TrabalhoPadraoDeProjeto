f1f9160ae99fa400818a1cad4099702c40e99520
Pedro <soufreitas.pedro@gmail.com>
2018-11-06 12:26:16 -0200

    Ajustando Tela de pedido

--- src/main/java/action/GravarProdutoPostAction.java
+++ src/main/java/action/GravarProdutoPostAction.java
@@ -1,14 +1,15 @@
-
 package action;
 
 import controller.Action;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Ingrediente;
 import model.Produto;
 import model.UsuarioRestaurante;
 import persistence.ProdutoDAO;
@@ -28,25 +29,31 @@ public class GravarProdutoPostAction implements Action {
             String nome = request.getParameter("nome");
             String tipo = request.getParameter("tipo");
             Double preco = Double.parseDouble(request.getParameter("preco").replace(",", "."));
-            UsuarioRestaurante restaurante = UsuarioDAO.getInstance().getUsuarioRestauranteByID((int)request.getSession().getAttribute("usuarioID"));
+            UsuarioRestaurante restaurante = UsuarioDAO.getInstance().getUsuarioRestauranteByID((int) request.getSession().getAttribute("usuarioID"));
             Produto produto = new Produto();
             produto.setPreco(preco).setDescricao(nome).setRestaurante(restaurante);
-            if(tipo.equals("PFINAL")){
+            if (tipo.equals("PFINAL")) {
                 produto.setIngrediente(false);
-            } else if(tipo.equals("INGREDIENTE")){
+                ProdutoDAO dao = ProdutoDAO.getINSTANCE();
+                Integer idProduto = dao.adicionar(produto, restaurante.getIdUsuario());
+
+                for (String value : values) {
+                    Integer idIngrediente = Integer.parseInt(value);
+                    ProdutoDAO.getINSTANCE().adicionarIngrediente(idProduto, idIngrediente);
+                }
+
+            } else if (tipo.equals("INGREDIENTE")) {
                 produto.setIngrediente(true);
+                ProdutoDAO dao = ProdutoDAO.getINSTANCE();
+                Integer idProduto = dao.adicionar(produto, restaurante.getIdUsuario());
             }
-            ProdutoDAO dao = ProdutoDAO.getINSTANCE();
-            Integer idProduto = dao.adicionar(produto, restaurante.getIdUsuario());
-            
-            for(String value : values){
-                Integer idIngrediente = Integer.parseInt(value);
-                ProdutoDAO.getINSTANCE().adicionarIngrediente(idProduto, idIngrediente);
-            }
-            response.sendRedirect("CadastroProduto.jsp");
+            List<Ingrediente> ingredientes = ProdutoDAO.getINSTANCE().
+                    getIngredientesByRestaurante((int)request.getSession().getAttribute("usuarioID"));
+            request.setAttribute("ingredientes", ingredientes);
+            request.getRequestDispatcher("CadastroProduto.jsp").forward(request,response);
         } catch (SQLException ex) {
             Logger.getLogger(GravarProdutoPostAction.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }

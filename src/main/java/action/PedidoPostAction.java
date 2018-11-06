601ca8e19bbd1b4b51357cab0feff44f3d34cabc
Pedro <soufreitas.pedro@gmail.com>
2018-11-06 16:55:14 -0200

    Consertando erros

--- src/main/java/action/PedidoPostAction.java
+++ src/main/java/action/PedidoPostAction.java
@@ -5,10 +5,35 @@
  */
 package action;
 
+import controller.Action;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Pedido;
+import persistence.PedidoDAO;
+import persistence.RestauranteDAO;
+import persistence.UsuarioDAO;
+
 /**
  *
  * @author pedrofreitas
  */
-public class PedidoPostAction {
+public class PedidoPostAction implements Action{
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
+        try {
+            Pedido pedido = (Pedido) request.getSession().getAttribute("pedido");
+            PedidoDAO dao = PedidoDAO.getInstance();
+            dao.adicionar(pedido);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PedidoPostAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }

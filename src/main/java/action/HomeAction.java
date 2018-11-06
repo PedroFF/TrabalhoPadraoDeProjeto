93847147eb91b05f2ac77281fddb348ba6c5b932
Pedro <soufreitas.pedro@gmail.com>
2018-11-06 11:49:06 -0200

    Consertando erros

--- src/main/java/action/HomeAction.java
+++ src/main/java/action/HomeAction.java
@@ -30,6 +30,7 @@ public class HomeAction implements Action {
             ListarRestauranteAction comando = new ListarRestauranteAction();
             comando.execute(request, response);
         } else if (tipo.equals("RESTAURANTE")) {
+            response.setContentType("text/html;charset=UTF-8");
             request.setAttribute("restaurante", restaurante);
             request.getRequestDispatcher("indexRestaurante.jsp").forward(request, response);
         }

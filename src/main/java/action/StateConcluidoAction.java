344ec8c496f9a7048b5b533b1cde58f456d25c66
Joao Paulo <jpdias1997@hotmail.com>
2018-11-06 03:06:03 -0200

    Finalização das Actions do Pedido

--- src/main/java/action/StateConcluidoAction.java
+++ src/main/java/action/StateConcluidoAction.java
@@ -3,9 +3,17 @@ package action;
 
 import controller.Action;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.EstadoNaoPermitidoException;
+import model.Pedido;
+import model.UsuarioRestaurante;
+import persistence.PedidoDAO;
+import persistence.UsuarioDAO;
 
 /**
  *
@@ -15,6 +23,18 @@ public class StateConcluidoAction implements Action {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
+        try {
+            Integer id = Integer.parseInt(request.getParameter("id"));
+            Integer idRestaurante = Integer.parseInt((String)request.getSession().getAttribute("usuarioID"));
+            UsuarioRestaurante restaurante = UsuarioDAO.getInstance().getUsuarioRestauranteByID(idRestaurante);
+            Pedido pedido = PedidoDAO.getInstance().getPedidoByIdByRestaurante(id,restaurante.getIdUsuario());
+            pedido.saveToMemento();
+            pedido.concluirPedido();
+            PedidoDAO.getInstance().adicionarHistorico(pedido, pedido.getStatus().getDescricao(), true); //Confirmar
+            PedidoDAO.getInstance().updateEstado(pedido);
+        } catch (SQLException | EstadoNaoPermitidoException ex) {
+            Logger.getLogger(StateConcluidoAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
       
     }
     

8810acd93e061b3fe9757164e47f4ea9b03593be
Joao Paulo <jpdias1997@hotmail.com>
2018-11-06 00:56:38 -0200

    Correção SQL

--- src/main/java/persistence/ProdutoDAO.java
+++ src/main/java/persistence/ProdutoDAO.java
@@ -19,7 +19,7 @@ public class ProdutoDAO {
 
     private Connection conexao;
     private static final ProdutoDAO INSTANCE = new ProdutoDAO();
-    private String SQL_INSERT_ITEM = "INSERT INTO item(nome,preco,fk_restaurante,ingrediente) VALUES (?,?,?,?,?)";
+    private String SQL_INSERT_ITEM = "INSERT INTO item(nome,preco,fk_restaurante,ingrediente) VALUES (?,?,?,?)";
     private String SQL_INSERT_ITEM_INGREDIENTE = "INSERT INTO INGREDIENTE(id_item_composto,id_item_componente) VALUES (?,?)";
     private String SQL_SELECT_ITEM_BY_ID_AND_RESTAURANTE = "SELECT * FROM ITEM WHERE ID = ? AND FK_RESTAURANTE = ?";
     private String SQL_SELECT_ALL_ITEMS_COMPOSTOS = "SELECT id_item FROM ITEM WHERE FK_RESTAURANTE = ? AND INGREDIENTE = FALSE";
